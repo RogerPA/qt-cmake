755b00082d46660d95bc3daf12a9e1421f9433af
Roger Peralta Aranibar <aranibar.roger@gmail.com>
2020-09-29 20:16:50 -0500

    :tada: changes in main

--- source/main.cpp
+++ source/main.cpp
@@ -7,6 +7,8 @@
 int main(int argc, char *argv[]) {
   QApplication app(argc, argv);
 
+  std::cout << "Hello world" << std::endl;
+
   std::string file_path = "input.txt";
 
   MainWindow mainWindow;
